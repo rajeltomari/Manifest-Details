75c0de23425082188e9bd2147dd030085f5b135e
Donald Davis <ddavis34@gmx.com>
2012-05-22 17:55:24 -0400

    SQL Dump for Manifest Details MOD
    Signed-off-by: Donald Davis <ddavis34@gmx.com>

---
+++ manifest_mod.sql
@@ -0,0 +1,14 @@
+-- --------------------------------
+-- MANIFEST DETAILS MODIFICATION --
+-- --------------------------------
+-- -------------------------------------------------
+-- Dumping data for table `nova_settings`
+-- -------------------------------------------------
+INSERT INTO `nova_settings` VALUES (NULL, 'modManifest_show_species', 'True', 'Manifest Details - Show Species', 'y');
+INSERT INTO `nova_settings` VALUES (NULL, 'modManifest_show_gender', 'True', 'Manifest Details - Show Gender', 'y');
+INSERT INTO `nova_settings` VALUES (NULL, 'modManifest_show_thumbnail', 'True', 'Manifest Details - Show Thumbnail', 'y');
+
+-- -------------------------------------------------
+-- Dumping data for table `nova_menu_items`
+-- -------------------------------------------------
+INSERT INTO `nova_menu_items` VALUES (NULL , 'Manifest Options', '0', '3', 'admin/modmanifest', 'onsite', 'none', 'y', 'site/settings', '0', 'adminsub', 'admin', 'y', '1');
